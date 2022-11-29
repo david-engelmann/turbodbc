00a4b2157e4e1f7fef4522b426f58da803e00c21
David Engelmann <david+4113@satisfilabs.com>
2022-11-29 09:21:35 -0500

    add additional test for nextset

--- python/turbodbc_test/test_cursor_nextset.py
+++ python/turbodbc_test/test_cursor_nextset.py
@@ -20,28 +20,6 @@ def test_nextset_with_one_result_set(dsn, configuration):
     else:
         assert True, "Found call for nextset"
 
-@for_one_result_set
-def test_nextset_with_two_result_set(dsn, configuration):
-    print(f"dsn: {dsn}\n")
-    cursor = connect(dsn, **get_credentials(configuration)).cursor()
-    multi_result_set_stored_proc = """CREATE PROCEDURE TEST_PROC 
-    AS 
-    SET NOCOUNT ON 
-    SELECT 4 
-    SELECT 2 
-    """
-    cursor.execute(multi_result_set_stored_proc)
-    cursor.execute("EXEC TEST_PROC;")
-    try:
-        assert cursor.fetchall() == [[4]]
-        next_set_present = cursor.nextset()
-        if next_set_present:
-            assert cursor.fetchall() == [[2]]
-    except Exception as exc:
-        assert False, f"Didn't find a call for nextset\n{exc}\n"
-    else:
-        assert True, "Found call for nextset"
-
 @for_one_database
 def test_nextset_with_function(dsn, configuration):
     cursor = connect(dsn, **get_credentials(configuration)).cursor()
@@ -66,30 +44,64 @@ def test_nextset_with_function(dsn, configuration):
 @for_one_database
 def test_nextset_with_postgres_procedure(dsn, configuration):
     cursor = connect(dsn, **get_credentials(configuration)).cursor()
-    multi_result_set_func = """CREATE PROCEDURE TEST_PROC(result_one inout refcursor, result_two inout refcursor) 
-    AS 
-    $$
-    BEGIN
-        OPEN result_one FOR SELECT 4;
+    multi_result_set_func = """CREATE PROCEDURE TEST_PROC(INOUT _result_one
+    REFCURSOR = 'result_one', INOUT _result_two REFCURSOR = 'result_two') 
+    LANGUAGE 'plpgsql'
+    AS $BODY$ BEGIN
+        OPEN _result_one FOR SELECT 4;
 
-        OPEN result_two FOR SELECT 2;
-    END;
-    $$ 
-    LANGUAGE plpgsql;
+        OPEN _result_two FOR SELECT 2;
+    END $BODY$;
     """
     cursor.execute(multi_result_set_func)
-    cursor.execute("select TEST_FUNC(null, null);")
+    multi_result_call_func = """
+    CALL TEST_PROC();
+    BEGIN;
+        FETCH ALL FROM "result_one";
+        FETCH ALL FROM "result_two";
+    COMMIT;
+    """
+    cursor.execute(multi_result_call_func)
     try:
-#        assert cursor.fetchall() == [[4], [2]]
-        assert True == True
-        print(f"fetch all for open proc:\n{cursor.fetchall()}\n")
+        assert cursor.fetchall() == [['result_one', 'result_two']]
     except Exception as exc:
         assert False, f"Didn't find a call for nextset\n{exc}\n"
     else:
         assert True, "Found call for nextset"
 
 @for_one_database
-def test_nextset_with_one_result_set(dsn, configuration):
+def test_nextset_with_postgres_function(dsn, configuration):
+    cursor = connect(dsn, **get_credentials(configuration)).cursor()
+    multi_result_set_func = """CREATE FUNCTION TEST_FUNC() RETURNS SETOF refcursor as $$
+    DECLARE
+        result_one refcursor := 'result_one';
+        result_two refcursor := 'result_two';
+    BEGIN
+        OPEN result_one FOR SELECT 4;
+        RETURN NEXT result_one;
+        OPEN result_two FOR SELECT 2;
+        RETURN NEXT result_two;
+    END;
+    $$ LANGUAGE plpgsql;
+    """
+    cursor.execute(multi_result_set_func)
+    multi_result_call_func = """
+    select * from TEST_FUNC();
+    BEGIN;
+        FETCH ALL FROM "result_one";
+        FETCH ALL FROM "result_two";
+    COMMIT;
+    """
+    cursor.execute(multi_result_call_func)
+    try:
+        assert cursor.fetchall() == [['result_one'], ['result_two']]
+    except Exception as exc:
+        assert False, f"Didn't find a call for nextset\n{exc}\n"
+    else:
+        assert True, "Found call for nextset"
+
+@for_one_database
+def test_nextset_with_two_select_statements(dsn, configuration):
     cursor = connect(dsn, **get_credentials(configuration)).cursor()
     cursor.execute("SELECT 4;SELECT 2;")
     try:
@@ -101,3 +113,74 @@ def test_nextset_with_one_result_set(dsn, configuration):
         assert False, f"Didn't find a call for nextset\n{exc}\n"
     else:
         assert True, "Found call for nextset"
+
+@for_one_result_set
+def test_nextset_with_two_result_set(dsn, configuration):
+    print(f"dsn: {dsn}\n")
+    cursor = connect(dsn, **get_credentials(configuration)).cursor()
+    multi_result_set_stored_proc = """CREATE PROCEDURE TEST_PROC 
+    AS 
+    SET NOCOUNT ON 
+    SELECT 4 
+    SELECT 2 
+    """
+    cursor.execute(multi_result_set_stored_proc)
+    cursor.execute("EXEC TEST_PROC;")
+    try:
+        assert cursor.fetchall() == [[4]]
+        next_set_present = cursor.nextset()
+        if next_set_present:
+            assert cursor.fetchall() == [[2]]
+    except Exception as exc:
+        assert False, f"Didn't find a call for nextset\n{exc}\n"
+    else:
+        assert True, "Found call for nextset"
+
+@for_one_result_set
+def test_nextset_with_two_result_set_with_alias(dsn, configuration):
+    print(f"dsn: {dsn}\n")
+    cursor = connect(dsn, **get_credentials(configuration)).cursor()
+    multi_result_set_stored_proc = """CREATE PROCEDURE TEST_PROC 
+    AS 
+    SET NOCOUNT ON 
+    SELECT 4 as Four 
+    SELECT 2 as Two
+    """
+    cursor.execute(multi_result_set_stored_proc)
+    cursor.execute("EXEC TEST_PROC;")
+    try:
+        assert cursor.fetchall() == [[4]]
+        next_set_present = cursor.nextset()
+        if next_set_present:
+            assert cursor.fetchall() == [[2]]
+    except Exception as exc:
+        assert False, f"Didn't find a call for nextset\n{exc}\n"
+    else:
+        assert True, "Found call for nextset"
+
+@for_one_result_set
+def test_nextset_with_three_result_set(dsn, configuration):
+    print(f"dsn: {dsn}\n")
+    cursor = connect(dsn, **get_credentials(configuration)).cursor()
+    multi_result_set_stored_proc = """CREATE PROCEDURE TEST_PROC 
+    AS 
+    SET NOCOUNT ON 
+    SELECT 4 
+    SELECT 3 
+    SELECT 2
+    """
+    cursor.execute(multi_result_set_stored_proc)
+    cursor.execute("EXEC TEST_PROC;")
+    try:
+        assert cursor.fetchall() == [[4]]
+        next_set_present = cursor.nextset()
+        if next_set_present:
+            assert cursor.fetchall() == [[3]]
+            next_set_present = cursor.nextset()
+            if next_set_present:
+                assert cursor.fetchall() == [[2]]
+    except Exception as exc:
+        assert False, f"Didn't find a call for nextset\n{exc}\n"
+    else:
+        assert True, "Found call for nextset"
+
