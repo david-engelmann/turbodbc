6be8658058a2e707a60c0b7a540447708e3d2a37
Uwe L. Korn <uwe.korn@quantco.com>
2023-02-07 17:33:08 +0100

    Search for numpy include in LIBRARY_PREFIX

--- cmake_scripts/Findnumpy.cmake
+++ cmake_scripts/Findnumpy.cmake
@@ -6,6 +6,7 @@ find_path(
     HINTS
         $ENV{VIRTUAL_ENV}/lib/*/site-packages/numpy/core/include
         $ENV{CONDA_PREFIX}/lib/*/site-packages/numpy/core/include
+        $ENV{CONDA_PREFIX}/Library/lib/*/site-packages/numpy/core/include
         ENV PYTHON_INCLUDE_DIR
         /usr/local/lib/python2.7/dist-packages/numpy/core/include
         $ENV{PYTHON}/lib/site-packages/numpy/core/include
